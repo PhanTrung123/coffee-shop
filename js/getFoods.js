56075cf9caef5ba5c5bcfb887a3a2e0ea0fa32f0
PhanTrung123 <phanquangtrung30042004@gmail.com>
2026-05-09 11:04:11 +0700

    up

--- js/getFoods.js
+++ js/getFoods.js
@@ -3,10 +3,12 @@ async function getDataFoods() {
 
   const foodsList = document.querySelector(".foods-list");
 
-  data.forEach((list) => {
-    const item = document.createElement("div");
-    item.classList.add("col");
-    item.innerHTML = `
+  data
+    .sort((a, b) => a.id - b.id)
+    .forEach((list) => {
+      const item = document.createElement("div");
+      item.classList.add("col");
+      item.innerHTML = `
               <div
                 class="card p-3"
               >
@@ -18,12 +20,12 @@ async function getDataFoods() {
                   </div>
                   <p class="mb-0 fw-bold">${list.name}</p>
                   <div class="foods-list-btns">
-                    <i class="fa-solid fa-pen-to-square"></i>
-                    <i class="fa-solid fa-trash-can"></i>
+                    <i onClick=handleEditFood(${list.id}) class="fa-solid text-primary fa-pen-to-square" data-bs-toggle="modal" data-bs-target="#foods"></i>
+                    <i onClick=getByIdFoods(${list.id}) class="fa-solid fa-trash-can" data-bs-toggle="modal" data-bs-target="#deleteFoods"></i>
                   </div>
                 </div>
                 <div class="m-auto w-50">
-                  <img src="${list.imgUrl}" alt="" />
+                  <img class="hieuung" src="${list.imgUrl}" alt="" />
                 </div>
                 <div class="price d-flex justify-content-center pt-2">
                   <span class="fw-bold ">${list.price}</span>
@@ -36,8 +38,8 @@ async function getDataFoods() {
 
               </div>
         `;
-        foodsList.appendChild(item);
-  });
+      foodsList.appendChild(item);
+    });
 }
 getDataFoods();
 
@@ -45,27 +47,79 @@ getDataFoods();
 
 const addFoodBtn = document.getElementById("addFoods");
 const imgFood = document.getElementById("productImg");
-let selectedFoodImageFile ;
- imgFood.addEventListener("change", handleFoodImageSelect);
+let selectedFoodImageFile;
+let idEdit;
+imgFood.addEventListener("change", handleFoodImageSelect);
 addFoodBtn.addEventListener("click", async () => {
   const foodName = document.getElementById("foodName");
   const price = document.getElementById("price");
   const imgUrl = await uploadImageToCloudinary(selectedFoodImageFile);
-  let idFood = 1 ;
+  let idFood = 1;
   const data = await getAll(URL_DISH);
-  data.sort((a,b) => a.id - b.id).forEach(e => {
-     if(idFood == e.id){
-       idFood++ ;
-     }else {
-         return; 
-     }
-  })
+  data
+    .sort((a, b) => a.id - b.id)
+    .forEach((e) => {
+      if (idFood == e.id) {
+        idFood++;
+      } else {
+        return;
+      }
+    });
   const newUpdate = {
-      "id" : "44",
-      "imgUrl": imgUrl,
-      "name": foodName.value,
-      "price": price.value
-    }
-  add(URL_DISH, newUpdate);
+    id: idEdit ? idEdit : idFood,
+    imgUrl: imgUrl,
+    name: foodName.value,
+    price: price.value,
+  };
+  if (idEdit) {
+    edit(URL_DISH, newUpdate);
+  } else {
+    add(URL_DISH, newUpdate);
   }
-)
+});
+
+function getByIdFoods(id) {
+  const showID = document.getElementById("idFood");
+  showID.innerText = id;
+}
+
+// --------------------------------
+const deleteFoodItem = document.getElementById("deleteFood");
+
+deleteFoodItem.addEventListener("click", () => {
+  const idFood = document.getElementById("idFood");
+  deleted(URL_DISH, idFood.innerText);
+});
+
+// --------------------------------
+async function handleEditFood(id) {
+  idEdit = id;
+  const data = await getAll(URL_DISH);
+  const food = data.find((e) => e.id == id);
+  const foodName = document.getElementById("foodName");
+  foodName.value = food.name;
+  const price = document.getElementById("price");
+  price.value = food.price;
+  const title = document.getElementById("title");
+  title.innerText = "Edit Info Food";
+  const img = document.getElementById("imgfood");
+  img.src = food.imgUrl;
+  const EditBtn = document.getElementById("addFoods");
+  EditBtn.innerText = "Edit Food";
+}
+
+// ---------------------------------
+const reset = document.querySelector(".add");
+ reset.addEventListener("click", () => {
+    const foodName = document.getElementById("foodName");
+  foodName.value = "";
+  const price = document.getElementById("price");
+  price.value = "";
+  const title = document.getElementById("title");
+  title.innerText = "Add Food";
+  const img = document.getElementById("imgfood");
+  img.src = "../images/logo.png";
+  const EditBtn = document.getElementById("addFoods");
+  EditBtn.innerText = "Add Food";
+ })
+
